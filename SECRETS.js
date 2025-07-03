c12b35984905c03a9b887a6690c869f01c43a401
jujulisewu <jujulisewu@gmail.com>
2025-07-03 15:43:04 +0700

    Create SECRETS.js

---
+++ SECRETS.js
@@ -0,0 +1,7 @@
+const SECRETS = {
+  APP_KEY: process.env.APP_KEY,
+  APP_SECRET: process.env.APP_SECRET,
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+  ACCESS_SECRET: process.env.ACCESS_SECRET,
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+};
